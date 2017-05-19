835662e2fbee899a3f64ead234f2953b52868e27
William Aguera <williamaguera.m@hotmail.com>
2017-05-19 16:29:21 -0300

    feat(input): Implementar Addon para Input, criar propriedades para aceitar addon dos dois lados.

--- src/input/input.ts
+++ src/input/input.ts
@@ -1,15 +1,18 @@
 /**
  * Created by William on 18/05/2017.
  */
-import {Component, OnInit, Input} from '@angular/core'
+import {Component, OnInit, Input, ViewEncapsulation} from '@angular/core'
 
 @Component({
     selector: 'tl-input',
+    encapsulation: ViewEncapsulation.None,
     templateUrl: './input.html',
-    styleUrls: ['./input.scss'],
+    styleUrls: ['./input.scss', '../assets/reset.css'],
 })
 export class TlInput implements OnInit {
     @Input() value: string = '';
+    @Input() iconLeft: string = '';
+    @Input() iconRight: string = '';
     @Input() placeholder: string = '';
 
     constructor() {

99c3319a2a50aa850767c7643e97659966f4ebf0
William Aguera <williamaguera.m@hotmail.com>
2017-05-22 10:28:08 -0300

    feat(input): Criar e Implementar controles para ngModel e registros de mudança.

--- src/input/input.ts
+++ src/input/input.ts
@@ -1,16 +1,34 @@
-/**
- * Created by William on 18/05/2017.
- */
-import {Component, OnInit, Input, ViewEncapsulation, ViewChild} from '@angular/core'
+import {Component, Input, ViewEncapsulation, ViewChild, forwardRef} from '@angular/core'
+import {NG_VALUE_ACCESSOR, ControlValueAccessor} from '@angular/forms';
+
+const noop = () => {};
+
+export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => TlInput),
+    multi: true
+};
 
 @Component({
     selector: 'tl-input',
-    encapsulation: ViewEncapsulation.None,
     templateUrl: './input.html',
     styleUrls: ['./input.scss', '../assets/reset.css'],
+    encapsulation: ViewEncapsulation.None,
+    providers: [CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR]
 })
-export class TlInput implements OnInit {
-    @Input() value: string = '';
+export class TlInput implements ControlValueAccessor {
+    @Input() ngValue: string = '';
+    get value(): any {
+        return this.ngValue;
+    }
+
+    set value(v: any) {
+        if (v !== this.ngValue) {
+            this.ngValue = v;
+            this.onChangeCallback(v);
+        }
+    }
+
     @Input() iconLeft: string = '';
     @Input() iconRight: string = '';
     @Input() placeholder: string = '';
@@ -18,16 +36,33 @@ export class TlInput implements OnInit {
 
     @ViewChild('input') input;
 
+    private onTouchedCallback: () => void = noop;
+    private onChangeCallback: (_: any) => void = noop;
+
     constructor() {
 
     }
 
-    ngOnInit() {
+    onBlur() {
+        this.onTouchedCallback();
+    }
 
+    writeValue(value: any) {
+        if (value !== this.ngValue) {
+            this.ngValue = value;
+        }
+    }
+
+    registerOnChange(fn: any) {
+        this.onChangeCallback = fn;
+    }
+
+    registerOnTouched(fn: any) {
+        this.onTouchedCallback = fn;
     }
 
     clearInput() {
         this.input.nativeElement.value = '';
-        this.value = '';
+        this.ngValue = '';
     }
 }

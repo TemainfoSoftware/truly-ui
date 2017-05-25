f0d1ccadeae63c2c3d32212bb63303a8ed24796a
William Aguera <williamaguera.m@hotmail.com>
2017-05-25 09:21:13 -0300

    feat(ComponentHasModel): Criar metodos de KeyboardEvent para aceitar navegação entre Input's.

--- src/core/util/ComponentHasModel.ts
+++ src/core/util/ComponentHasModel.ts
@@ -1,14 +1,20 @@
-import { ControlValueAccessor } from '@angular/forms';
-import { ComponentCustom } from "./ComponentCustom";
+import {ElementRef, Input} from "@angular/core";
+import {ControlValueAccessor,} from '@angular/forms';
+import {ComponentCustom} from "./ComponentCustom";
 import {ComponentTabIndexGenerator} from "./ComponentTabIndexGenerator";
-import {ElementRef} from "@angular/core";
 
 /**
  * A constant to be used in a function that performs no operations.
  */
-const noop = () => {};
+const noop = () => {
+};
 
 export class ComponentHasModel extends ComponentCustom implements ControlValueAccessor {
+    /**
+     * Controller to define if the tabulation is with key Enter or key Tab.
+     * @type {boolean}
+     */
+    @Input() enterAsTab: boolean = true;
 
     public tabIndex: ComponentTabIndexGenerator;
     /**
@@ -38,7 +44,10 @@ export class ComponentHasModel extends ComponentCustom implements ControlValueAc
 
     public setTabIndex(element: ElementRef) {
         this.tabIndex = new ComponentTabIndexGenerator(element);
+        this.setNextTabIndex(this.element.nativeElement.tabIndex + 1);
+        this.setPreviousTabIndex(this.element.nativeElement.tabIndex - 1);
     }
+
     /**
      * Callback of control value accessor to register touched changes
      */
@@ -74,6 +83,24 @@ export class ComponentHasModel extends ComponentCustom implements ControlValueAc
      */
     registerOnTouched(callback: any) {
         this.onTouchedCallback = callback;
+    }
+
+    onKeyInput(event: KeyboardEvent) {
+        if (this.enterAsTab) {
+            if (event.keyCode === 13 || event.keyCode === 40) {
+                this.nextFocus();
+            } else if (event.keyCode === 38) {
+                this.previousFocus();
+            }
+        }
+    }
+
+    previousFocus() {
+        document.getElementById('tl-' + this.element.nativeElement.localName + '-' + this.previousTabIndex).focus();
+    }
+
+    nextFocus() {
+        document.getElementById('tl-' + this.element.nativeElement.localName + '-' + this.nextTabIndex).focus();
     }
 
 }

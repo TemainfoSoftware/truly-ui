94fb8248ebe231b5182bdffd1f16216a624bad69
William Aguera <williamaguera.m@hotmail.com>
2017-05-25 09:19:06 -0300

    refactor(ComponentIDGenerator): Alterar nome de variavel incorreta.

--- src/core/util/ComponentIDGenerator.ts
+++ src/core/util/ComponentIDGenerator.ts
@@ -2,11 +2,11 @@ import {ElementRef} from "@angular/core";
 /**
  * Created by William on 24/05/2017.
  */
-let nexUniqueId = 0;
+let nextUniqueId = 0;
 
 export class ComponentIDGenerator {
 
     constructor(element: ElementRef) {
-        element.nativeElement.id = 'tl-' + element.nativeElement.localName + '-' + nexUniqueId++;
+        element.nativeElement.id = 'tl-' + element.nativeElement.localName + '-' + nextUniqueId++;
     }
 }

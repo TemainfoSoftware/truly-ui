efcdb93cacb337c9c570f94bcc9ca869fc56cf39
Maicon Wagner dos Santos <mister-x59@hotmail.com.br>
2017-06-02 09:01:23 -0300

    test(input): Adicionar testes para validar propriedades
    Adicionado testes para validar as propriedades CLEARBUTTON, LABEL, LABELSIZE, LABELPLACEMENT

--- src/input/input.spec.ts
+++ src/input/input.spec.ts
@@ -1,35 +1,97 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { TlInput } from './input';
 import { InputModule } from './index';
+import { DebugElement } from '@angular/core';
 
-describe('TLInput Component', () => {
-    let comp :    TlInput;
-    let fixture : ComponentFixture<TlInput>;
+let componentInputInstance : TlInput;
+let elementoHTMLLabel :      HTMLElement;
+let fixture :                ComponentFixture<TlInput>;
+let debugElment :            DebugElement;
+let clearButton :            DebugElement;
+let inputElement :           HTMLInputElement;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            imports : [
-                InputModule
-            ]
-        });
+const textMock = 'Eu sou um Texto do Input';
 
-        fixture = TestBed.createComponent(TlInput);
-        comp = fixture.componentInstance;
-    });
+// async beforeEach
+beforeEach(() => {
+    TestBed.configureTestingModule({ imports : [ InputModule ] }).compileComponents();
+});
 
+// synchronous beforeEach
+beforeEach(() => {
+    fixture = TestBed.createComponent(TlInput);
+    componentInputInstance = fixture.componentInstance;
+});
+
+describe('TLInput (templateUrl)', () => {
     test('should instantiate the component', () => {
         expect(fixture.debugElement).toBeDefined();
     });
 
     test('deve inicialziar labelPlacement sempre a esquerda', () => {
-        const app = fixture.debugElement.componentInstance;
-        expect(app.labelPlacement).toEqual('left');
+        expect(componentInputInstance.labelPlacement).toEqual('left');
     });
 
-    test('deve inicialziar iconAfter sempre a esquerda', () => {
-        const app = fixture.debugElement.componentInstance;
-        expect(app.iconAfter).toEqual('');
+    test('deve incializar iconAfter sempre com vazio', () => {
+        expect(componentInputInstance.iconAfter).toEqual('');
     });
+});
+describe('TLInput Propriedade [ CLEARBUTTON ]', () => {
+
+    beforeEach(() => {
+        componentInputInstance.ngValue = textMock;
+        componentInputInstance.clearButton = true;
+        fixture.detectChanges();
+        clearButton = fixture.debugElement.query(By.css('.addon.-clearbutton'));
+        inputElement = fixture.debugElement.query(By.css('input')).nativeElement;
+    });
+
+    test('deve limpar elemento html "input" ao clicar no "ClearButton"', async(() => {
+        clearButton.triggerEventHandler('click', null);
+        expect( inputElement.value ).toEqual( '' );
+    }));
+
+    test('deve limpar propriedade "ngValue" ao clicar no "ClearButton"',  async(() => {
+        clearButton.triggerEventHandler('click', null);
+        expect( componentInputInstance.ngValue ).toEqual('');
+    }));
+});
+describe('TlInput Propriedade [ LABEL, LABELSIZE, LABELPLACEMENT ]', () => {
+
+    beforeEach(() => {
+        componentInputInstance.label = textMock;
+        fixture.detectChanges();
+        debugElment = fixture.debugElement.query(By.css('label'));
+        elementoHTMLLabel = debugElment.nativeElement;
+    });
+
+    test('deve posicionar o label a "esquerda" por padrão', async(() => {
+        expect( elementoHTMLLabel.className ).toContain('labelleft');
+    }));
+
+    test('deve posicionar o label "acima" do input quando setar a propriedade "labelPlacement"="top"', async(() => {
+        componentInputInstance.labelPlacement = 'top';
+        fixture.detectChanges();
+        expect( elementoHTMLLabel.className ).toContain('labeltop');
+    }));
+
+    test('deve exibir o texto "Eu sou um Texto do Input" no label ', async(() => {
+         fixture.detectChanges();
+         expect( elementoHTMLLabel.textContent ).toContain(textMock);
+    }));
+
+    test('deve exibir o elemento "label" com width setado em "LabelSize"', async(() => {
+        componentInputInstance.labelSize = '350';
+        fixture.detectChanges();
+        expect( elementoHTMLLabel.style.width ).toEqual('350px');
+    }));
+
+    test('não deve existir elemento label propriedade "label" for vazio',  async(() => {
+        componentInputInstance.label = '';
+        fixture.detectChanges();
+        debugElment = fixture.debugElement.query(By.css('label'));
+        expect( debugElment ).toBeNull();
+    }));
 });

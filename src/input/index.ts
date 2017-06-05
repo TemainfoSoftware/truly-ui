0ef13f7bd55f612d35a4e5334a88edbc9172a683
William Aguera <williamaguera.m@hotmail.com>
2017-06-05 16:59:10 -0300

    feat(field-mask): Criar e implementar diretiva para suportar mascara no input.

--- src/input/index.ts
+++ src/input/index.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TlInput } from './input';
+import { FieldMask } from '../core/directives/field-mask';
 
 export * from './input';
 
@@ -12,9 +13,11 @@ export * from './input';
     ],
     declarations: [
         TlInput,
+        FieldMask
     ],
     exports: [
         TlInput,
+        FieldMask
     ]
 })
 export class InputModule {}

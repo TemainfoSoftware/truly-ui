31cdb450cce99f22f320bd07b69fdc1ab68ac21f
Silvio Fleming <silvio@temainfo.com.br>
2017-07-04 10:39:48 -0300

    merge(*): Merge files

--- demo/src/app/app-routing.module.ts
+++ demo/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from "app/home/home.component";
       {path: 'input', loadChildren: './components/input/inputdemo.module#InputDemoModule'},
       {path: 'inputmask', loadChildren: './components/inputmask/inputmaskdemo.module#InputMaskDemoModule'},
       {path: 'button', loadChildren: './components/button/buttondemo.module#ButtonDemoModule'},
+      {path: 'splitbutton', loadChildren: './components/splitbutton/splitbuttondemo.module#SplitButtonDemoModule'},
       {path: 'modal', loadChildren: './components/modal/modaldemo.module#ModalDemoModule'},
       {path: 'tooltip', loadChildren: './components/tooltip/tooltipdemo.module#TooltipDemoModule'},
       {path: 'datatable', loadChildren: './components/datatable/datatabledemo.module#DatatableDemoModule'}

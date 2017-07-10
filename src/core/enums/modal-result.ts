98e1321dcbb0020caca5a68cb4058271bfbbd524
William Aguera <williamaguera.m@hotmail.com>
2017-07-10 14:17:45 -0300

    feat(modal-result): Create a enum to control Modal Results.

---
+++ src/core/enums/modal-result.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by William on 10/07/2017.
+ */
+export enum ModalResult {
+    MRFREE = -1,
+    MRCUSTOM = 0,
+    MROK = 1,
+    MRCANCEL = 2,
+    MRABORT = 3,
+    MRRETRY = 4,
+    MRIGNORE = 5,
+    MRYES = 6,
+    MRNO = 7,
+    MRCLOSE = 8,
+}

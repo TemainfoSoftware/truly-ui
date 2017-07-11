16a87c59ee14e75fc2e55d4d129f62bd2dde90e2
William Aguera <williamaguera.m@hotmail.com>
2017-07-11 10:12:05 -0300

    feat(modal): Create interface for modal options.

---
+++ src/modal/modal-options.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by William on 10/07/2017.
+ */
+export interface ModalOptions {
+    icon: string;
+    title: string;
+    color?: string;
+    height?: number;
+    width?: number;
+    draggable?: boolean;
+    maximizable?: boolean;
+    minimizable?: boolean;
+}

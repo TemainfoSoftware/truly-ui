427733a0d69963b87ece6637c5ff17f5ea44da06
Silvio Fleming <silvio@temainfo.com.br>
2017-07-13 18:10:05 -0300

    refactor(buttongroup): Add checkitem for default selected item

--- demo/src/app/components/buttongroup/buttongroupdemo.component.ts
+++ demo/src/app/components/buttongroup/buttongroupdemo.component.ts
@@ -16,20 +16,30 @@ export class ButtonGroupDemo {
 
   public itemSelected2: any[] = [];
 
+  public itemSelected3: any[] = [];
+
   constructor() {
     this.dataTableProperties = json.dataProperties;
   }
 
-  showItemSelected(event){
+  showItemSelected(event) {
     this.itemSelected = [];
     if(event.length > 0 && (event[0].indexSelected === true)){
       this.itemSelected.push( 'text: ' + event[0].text, 'index: ' + event[0].index );
     }
   }
+
   showItemSelected2(event){
     this.itemSelected2 = [];
     event.forEach( (item) => {
       this.itemSelected2.push( 'text: ' + item.text, 'index: ' + item.index );
+    });
+  }
+
+  showItemSelected3(event){
+    this.itemSelected3 = [];
+    event.forEach( (item) => {
+      this.itemSelected3.push( 'text: ' + item.text, 'index: ' + item.index );
     });
   }
 

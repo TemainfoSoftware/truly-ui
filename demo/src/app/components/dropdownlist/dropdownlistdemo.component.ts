bc0fb57835ec8365430f83fc3a6e8dae9d8e4bff
Silvio Fleming <silvio@temainfo.com.br>
2017-07-26 09:36:38 -0300

    docs(dropdownlist): Add placeholder and data items

--- demo/src/app/components/dropdownlist/dropdownlistdemo.component.ts
+++ demo/src/app/components/dropdownlist/dropdownlistdemo.component.ts
@@ -4,20 +4,62 @@ import * as json from './dropdownlistdemo-dataproperties.json';
 
 
 @Component( {
-  selector: 'app-button-group',
-  templateUrl: './dropdownlistdemo.component.html',
-  styleUrls: [ './dropdownlistdemo.component.scss' ]
+  selector : 'app-button-group',
+  templateUrl : './dropdownlistdemo.component.html',
+  styleUrls : [ './dropdownlistdemo.component.scss' ]
 } )
 export class DropDownListDemo {
 
   private dataTableProperties;
 
+  public itemSelected: any[];
 
+  public itemSelected2: any[];
 
+  public data: any[];
 
   constructor() {
     this.dataTableProperties = json.dataProperties;
+    this.data = [
+      {
+        textItem : 'Item 1',
+        valueItem : '1'
+      },
+      {
+        textItem : 'Item 2',
+        valueItem : '2'
+      },
+      {
+        textItem : 'Item 3',
+        valueItem : '3'
+      },
+      {
+        textItem : 'Item 4',
+        valueItem : '4'
+      }
+      ,
+      {
+        textItem : 'Item 5',
+        valueItem : '5'
+      }
+      ,
+      {
+        textItem : 'Item 6',
+        valueItem : '6'
+      }
+    ];
   }
+
+  showItemSelected( event ) {
+    this.itemSelected = [];
+    this.itemSelected.push( ' ' + event.textItem );
+  }
+
+  showItemSelected2( event ) {
+    this.itemSelected2 = [];
+    this.itemSelected2.push( ' ' + event.textItem );
+  }
+
 
 
 }

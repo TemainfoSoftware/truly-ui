2bd6a97c345b627726e4d60ee47d8ad3b7af2a0d
William Aguera <williamaguera.m@hotmail.com>
2017-07-28 14:29:10 -0300

    refactor(component-default-base, component-has-model-base): Reallocate responsibilities of methods.

--- src/core/base/component-default.base.ts
+++ src/core/base/component-default.base.ts
@@ -19,30 +19,34 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  SOFTWARE.
  */
-import { ElementRef } from '@angular/core';
+import { ElementRef, Input } from '@angular/core';
 import { IdGeneratorService } from '../helper/idgenerator.service';
 import { NameGeneratorService } from '../helper/namegenerator.service';
+import { TabIndexService } from '../../form/tabIndex.service';
+import { KeyEvent } from '../enums/key-events';
 
 /**
  * Class extended of others components, in charge of generate ID and TabIndex.
  */
 export class ComponentDefaultBase {
     /**
+     * Controller to define if the tabulation is with key Enter or key Tab.
+     * @type {boolean}
+     */
+    @Input() enterAsTab = true;
+
+    /**
      * The element itself.
      */
     public element: ElementRef;
 
     /**
-     * Variable to calculate what's the next tab index.
+     * TabIndex of Element;
      */
-    public nextTabIndex: number;
+    public tabindex: number;
 
-    /**
-     * Variable to calculate what's the previous tab index.
-     */
-    public previousTabIndex: number;
 
-    constructor(public idService: IdGeneratorService, public nameService: NameGeneratorService) {}
+    constructor(public tabIndexService: TabIndexService, public idService: IdGeneratorService, public nameService: NameGeneratorService) {}
 
     /**
      * @param value The element received of the components.
@@ -54,19 +58,97 @@ export class ComponentDefaultBase {
         this.nameService.createName( value, name );
     }
 
+
     /**
-     * Function that nextabIndex receive the value;
-     * @param value
+     * Function to set tabIndex of Elements received.
+     * @param element
      */
-    public setNextTabIndex( value: number ) {
-        this.nextTabIndex = value;
+    setTabIndex( element: ElementRef ) {
+        setTimeout( () => {
+            this.tabindex = this.tabIndexService.setTabIndex( element );
+        }, 1 );
     }
 
     /**
-     * Function that previousTabIndx receive the value;
-     * @param value
+     * Function that trigger a keyinput in element.
+     * @param event
      */
-    public setPreviousTabIndex( value: number ) {
-        this.previousTabIndex = value;
+    onKeyInput( event: KeyboardEvent ) {
+        if ( this.enterAsTab ) {
+            if (event.keyCode === KeyEvent.TAB && event.shiftKey) {
+                event.preventDefault();
+                this.previousFocus();
+                return;
+            }
+            switch ( event.keyCode ) {
+                case KeyEvent.ENTER:
+                    this.nextFocus();
+                    break;
+                case KeyEvent.ARROWDOWN:
+                    this.nextFocus();
+                    break;
+                case KeyEvent.ARROWUP:
+                    this.previousFocus();
+                    break;
+                case KeyEvent.TAB:
+                    event.preventDefault();
+                    this.nextFocus();
+                    break;
+            }
+        }
     }
+
+    /**
+     * Function to set focus on previous element
+     */
+    previousFocus() {
+        const previousElement = this.getPreviousElementOnForm();
+        if ( previousElement !== undefined ) {
+            (previousElement as HTMLElement).focus();
+        }
+    }
+
+    /**
+     * Function to set focus on next element
+     */
+    nextFocus() {
+        const nextElement = this.getNextElementOnForm();
+        if ( nextElement !== undefined ) {
+            (nextElement as HTMLElement).focus();
+        }
+    }
+
+    getNextElementOnForm() {
+        const form = document.querySelectorAll( 'tl-form' );
+        for ( let child = 0; child < form.length; child++ ) {
+            const doc = form[ child ].querySelectorAll( '*' );
+            for ( let child2 = 0; child2 < doc.length; child2++ ) {
+                if ( this.isNextTabIndex( doc, child2 ) ) {
+                    return doc[ child2 ];
+                }
+            }
+        }
+    }
+
+    getPreviousElementOnForm() {
+        const form = document.querySelectorAll( 'tl-form' );
+        for ( let child = 0; child < form.length; child++ ) {
+            const doc = form[ child ].querySelectorAll( '*' );
+            for ( let child2 = 0; child2 < doc.length; child2++ ) {
+                if ( this.isPreviousTabIndex( doc, child2 ) ) {
+                    return doc[ child2 ];
+                }
+            }
+        }
+    }
+
+    isNextTabIndex( doc, child2 ) {
+        return doc[ child2 ].tabindex === this.tabindex + 1;
+    }
+
+    isPreviousTabIndex( doc, child2 ) {
+        return doc[ child2 ].tabindex === this.tabindex - 1;
+    }
+
+
 }

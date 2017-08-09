ef34b664c371451e520ddee3a9cbaa960797ce54
William Aguera <williamaguera.m@hotmail.com>
2017-08-09 17:12:36 -0300

    docs(showcase): Create home page.

--- demo/src/app/home/home.component.ts
+++ demo/src/app/home/home.component.ts
@@ -1,13 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
   title = 'app works!';
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
+
+  ngAfterViewInit() {
+  }
 }

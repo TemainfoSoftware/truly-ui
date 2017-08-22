d2f42e8cd237e339223e785f9ef9aaa46e260750
William Aguera <williamaguera.m@hotmail.com>
2017-08-22 13:55:22 -0300

    docs(showcase): Change docs of radio button

--- demo/src/app/components/radiobutton/radiobuttondemo-dataproperties.json.ts
+++ demo/src/app/components/radiobutton/radiobuttondemo-dataproperties.json.ts
@@ -3,74 +3,31 @@
  */
 export const dataProperties = [
   {
-    name: "iconBefore",
-    type: "string",
-    default: "null",
-    description: "Create an icon Before the Input.",
-    options: "ion-printer | fa fa-home | any"
-  },
-  {
-    name: "iconAfter",
-    type: "string",
-    default: "null",
-    description: "Create an icon After the Input.",
-    options: "ion-printer | fa fa-home | any"
-  },
-  {
     name: "label",
     type: "string",
     default: "null",
-    description: "Create a label together with Input Element",
-    options: "Any Text"
+    description: "Label of Radio Button",
+    options: "any text"
   },
   {
-    name: "labelPlacement",
-    type: "string",
-    default: "left",
-    description: "Label Position",
-    options: "top | left"
+    name: "value",
+    type: "string | number",
+    default: "null",
+    description: "Value of Radio Button that going be returned to model",
+    options: "any text"
   },
   {
-    name: "clearButton",
-    type: "boolean",
-    default: "false",
-    description: "Display an icon to clear any Input Value",
-    options: "Any Text"
-  },
-  {
-    name: "disabled",
-    type: "boolean",
-    default: "false",
-    description: "Display an input with not selectable text (disabled)",
-    options: "Any Text"
-  },
-  {
-    name: "readonly",
-    type: "boolean",
-    default: "false",
-    description: "Display an input with selectable text (only read)",
-    options: "Any Text"
-  },
-  {
-    name: "placeholder",
+    name: "nameGroup",
     type: "string",
     default: "null",
-    description: "Display a help text on Input",
+    description: "Name of group of Radio Button",
     options: "Any Text"
   },
   {
-    name: "textBefore",
+    name: "orientation",
     type: "string",
-    default: "null",
-    description: "Display a text Before the Input",
-    options: "Any Text"
-  }
-  ,
-  {
-    name: "textAfter",
-    type: "string",
-    default: "null",
-    description: "Display a text After the Input",
-    options: "Any Text"
+    default: "horizontal",
+    description: "Orientation of the Radios",
+    options: "vertical | horizontal"
   }
   ];

b22ca73950e3aab3cf2632987648b47661581fe3
William Aguera <williamaguera.m@hotmail.com>
2017-09-06 10:47:51 -0300

    docs(showcase): Update the modal docs showcase (Properties and Events) (#46)
    close #46

--- demo/src/app/components/modal/modaldemo.component.ts
+++ demo/src/app/components/modal/modaldemo.component.ts
@@ -3,7 +3,9 @@ import { ModalService } from "../../../../../src/modal/modal.service";
 import { routerTransition } from "../../router.animations";
 import { ModalOptions } from "../../../../../src/modal/modal-options";
 import { NewModal } from "./newmodal/newModal.component";
+
 import * as json from './modal-dataproperties.json';
+import * as jsonEvt from './modal-dataevents.json';
 
 
 @Component( {
@@ -17,11 +19,16 @@ export class ModalDemo {
   public index: number;
   public modals;
   public modalOptions: ModalOptions;
+
   private modalprop;
+  private modalevts;
   private modalResult;
+
 
   constructor(private view: ViewContainerRef, private modalService: ModalService) {
     this.modalService.setView(this.view);
+
+    this.modalevts = jsonEvt.dataEvents;
     this.modalprop = json.dataProperties;
 
     this.modalOptions = {

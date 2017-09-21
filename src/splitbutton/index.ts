b5387b1cdaa30dce4e2ff8349b4d3a631311387b
Maicon Wagner dos Santos <mister-x59@hotmail.com.br>
2017-09-21 09:15:52 -0300

    fix(*): fix Error Cannot use 'in' operator to search for 'providers' (#67)

--- src/splitbutton/index.ts
+++ src/splitbutton/index.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ButtonModule } from '../button';
+import { ButtonModule } from '../button/index';
 import { TlSplitButton } from './splitbutton';
 import { TlSplitButtonAction } from './splitbutton-action';
 

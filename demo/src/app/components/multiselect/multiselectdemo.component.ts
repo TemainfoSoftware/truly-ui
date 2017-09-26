8561793a18ea62ba4391684681f96e7b2f441021
William Aguera <williamaguera.m@hotmail.com>
2017-09-26 16:18:45 -0300

    docs(multiselect): Update the multiselect documentation.

--- demo/src/app/components/multiselect/multiselectdemo.component.ts
+++ demo/src/app/components/multiselect/multiselectdemo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
 
 import * as jsonProp from './multiselectdemo-dataproperties.json';
 import * as jsonEvt from './multiselectdemo-events.json';
@@ -10,15 +10,22 @@ import { DumpDataService } from "../../shared/services/dumpdata";
   styleUrls: [ './multiselectdemo.component.scss' ],
   providers: [ DumpDataService ]
 } )
-export class MultiSelectDemo {
+export class MultiSelectDemo implements OnChanges {
 
   private dataTableProperties;
+
   private events;
+
   private dataBasic = [];
+
+  private dataCustom = [];
+
   private dataBasicModel;
 
   constructor() {
+
     this.dataTableProperties = jsonProp.dataProperties;
+
     this.events = jsonEvt.events;
 
     this.dataBasic =
@@ -31,8 +38,26 @@ export class MultiSelectDemo {
         ,
       ];
 
-    this.dataBasicModel = [{ source: { id: 1, firstName: 'Fred', lastName: 'King', email: 'contact@domain.com', }, effect: { icon: 'ion-gear-a', color: '#dd6c6c' } },
-      { source: { id: 1, firstName: 'Laura', lastName: 'King', email: 'contact@domain.com', }, effect: { icon: 'ion-gear-a', color: '#dd6c6c' } }]
+    this.dataCustom =
+      [
+        { source: { id: 1, firstName: 'Antonio', lastName: 'King', email: 'contact@domain.com', } },
+        { source: { id: 1, firstName: 'Junior', lastName: 'King', email: 'contact@domain.com', } },
+        { source: { id: 1, firstName: 'Murilo', lastName: 'King', email: 'contact@domain.com', } },
+        { source: { id: 1, firstName: 'Sara', lastName: 'King', email: 'contact@domain.com', } },
+        { source: { id: 1, firstName: 'Claudia', lastName: 'King', email: 'contact@domain.com', } }
+        ,
+      ];
+
+    this.dataBasicModel = [
+      {
+        source: { id: 1, firstName: 'Fred', lastName: 'King', email: 'contact@domain.com', },
+        effect: { icon: 'ion-gear-a', color: '#dd6c6c' }
+      },
+      {
+        source: { id: 1, firstName: 'Laura', lastName: 'King', email: 'contact@domain.com', },
+        effect: { icon: 'ion-gear-a', color: '#dd6c6c' }
+      }
+    ]
 
   }
 
@@ -51,6 +76,10 @@ export class MultiSelectDemo {
 
   onRemoveTag($event) {
     console.log('Removed Tag', $event);
+  }
+
+  ngOnChanges( change : SimpleChanges ) {
+    console.log( 'changes', change );
   }
 
 }

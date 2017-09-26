9cd0704d9cd687a19107018eb8d73bb1cb33366a
William Aguera <williamaguera.m@hotmail.com>
2017-09-26 16:35:58 -0300

    docs(multiselect): Add new events to multiselect documentation.

--- demo/src/app/components/multiselect/multiselectdemo-events.json.ts
+++ demo/src/app/components/multiselect/multiselectdemo-events.json.ts
@@ -3,4 +3,12 @@ export const events = [
     name: "getSelecteds",
     description: "Dispacthed when a tag is selected or removed.",
   },
+  {
+    name: "onClickTag",
+    description: "Dispacthed when a tag is clicked.",
+  },
+  {
+    name: "onRemoveTag",
+    description: "Dispacthed when a tag is removed.",
+  },
 ];

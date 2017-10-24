f9ead9582e75b95710f188a0d71415c7a248f712
William Aguera <williamaguera.m@hotmail.com>
2017-10-24 09:03:32 -0200

    fix(value-acessor-provider): Fix return of the function MakeProvider broking build.

--- src/core/base/value-accessor-provider.ts
+++ src/core/base/value-accessor-provider.ts
@@ -22,7 +22,7 @@
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { forwardRef } from '@angular/core';
 
-export function MakeProvider( type: any) {
+export function MakeProvider( type: any): any {
     return {
         provide: NG_VALUE_ACCESSOR,
         useExisting: forwardRef(() => type),

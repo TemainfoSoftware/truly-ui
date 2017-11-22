85a6404f20fd9497d774217f0166cdb4db06e32b
Maicon Wagner dos Santos <mister-x59@hotmail.com.br>
2017-11-22 18:00:19 -0200

    docs(datatable): improvements in documentation of datatable

--- src/app/showcase/components/datatable/overview/datatabledemo-overview-dataevents.json.ts
+++ src/app/showcase/components/datatable/overview/datatabledemo-overview-dataevents.json.ts
@@ -31,14 +31,28 @@ export const dataProperties = [
     description: 'Callback to invoke when pagination occurs.'
   },
   {
-    name: 'dataCHange',
+    name: 'dataChange',
     parameters: [
       { event: 'event.skip', description: 'First row offset ' },
       { event: 'event.take', description: 'Number of rows per page ' }
     ],
     description: 'Callback to invoke when paging, sorting or filtering happens in infinite mode.'
+  },
+  {
+    name: 'filterData',
+    parameters: [
+      { event: 'event.filters', description: 'Data from the filter that was created ' }
+    ],
+    description: 'Callback to invoke when Data from the filter that was created.'
   }
   ,
+  {
+    name: 'sortData',
+    parameters: [
+      { event: 'event.sorts', description: 'Ordering data that was created ' }
+    ],
+    description: 'Callback to invoke when ordering data that was created.'
+  },
   {
     name: 'endRow',
     parameters: [

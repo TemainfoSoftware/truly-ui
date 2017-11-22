faf1477d2eed1632ed976286be3829bbb6e21ef3
William Aguera <williamaguera.m@hotmail.com>
2017-11-22 11:54:00 -0200

    styl(form, showcasecard): lint of files.

--- src/app/showcase/components/showcase-card/showcase-card.module.ts
+++ src/app/showcase/components/showcase-card/showcase-card.module.ts
@@ -27,8 +27,8 @@ import { HighlightJsModule } from 'ngx-highlight-js';
 import { PanelGroupModule } from '../../../components/panelgroup';
 import { RadioButtonModule } from '../../../components/radiobutton';
 import { InputModule } from '../../../components/input';
-import { ShowcaseCardComponent } from "./showcase-card.component";
-import { CardComponent } from "./card/card.component";
+import { ShowcaseCardComponent } from './showcase-card.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [

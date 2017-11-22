a06be8ffd08913a3757b57ce96cb276acb2d3260
William Aguera <williamaguera.m@hotmail.com>
2017-11-22 15:00:51 -0200

    docs(showcard): refactor aligments of showcard.

--- src/app/showcase/components/showcase-card/card/card.component.ts
+++ src/app/showcase/components/showcase-card/card/card.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class CardComponent implements OnInit {
+
+  @ViewChild('infoBox') infoBox;
 
   constructor() { }
 
--- src/app/showcase/components/showcase-card/showcase-card.component.ts
+++ src/app/showcase/components/showcase-card/showcase-card.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
+import {
+  Component, OnInit, Input, ContentChildren, forwardRef, QueryList, AfterViewInit,
+  Renderer2
+} from '@angular/core';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-showcase-card',
   templateUrl: './showcase-card.component.html',
   styleUrls: ['./showcase-card.component.scss'],
 })
-export class ShowcaseCardComponent implements OnInit {
+export class ShowcaseCardComponent implements AfterViewInit {
+
+  @Input( 'icon' ) icon = '';
 
   @Input('title') title = 'New Card Header';
 
-  constructor() { }
+  @ContentChildren( forwardRef( () => CardComponent ) ) cardComponent: QueryList<CardComponent>;
 
-  ngOnInit() {
+  constructor(private renderer: Renderer2) { }
+
+  ngAfterViewInit() {
+    this.renderer.setStyle(this.cardComponent.last.infoBox.nativeElement, 'margin-bottom', '20px');
   }
 
 }

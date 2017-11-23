a983ab3a8daca6b60bc59cafb40add7b3f156fea
William Aguera <williamaguera.m@hotmail.com>
2017-11-23 14:27:14 -0200

    docs(panel): update documentation.

--- src/app/showcase/components/panel/paneldemo.module.ts
+++ src/app/showcase/components/panel/paneldemo.module.ts
@@ -8,6 +8,9 @@ import { HighlightJsModule } from 'ngx-highlight-js';
 import { PanelGroupModule } from '../../../components/panelgroup';
 import { RadioButtonModule } from '../../../components/radiobutton';
 import { InputModule } from '../../../components/input';
+import { ShowcaseCardModule } from '../../shared/components/showcase-card/showcase-card.module';
+import { ShowcaseTablePropertiesModule } from '../../shared/components/showcase-table-properties/showcase-table-properties.module';
+import { ShowcaseTableEventsModule } from '../../shared/components/showcase-table-events/showcase-table-events.module';
 
 @NgModule({
   declarations: [
@@ -21,6 +24,9 @@ import { InputModule } from '../../../components/input';
     InputModule,
     PanelDemoRoutingModule,
     HighlightJsModule,
+    ShowcaseCardModule,
+    ShowcaseTablePropertiesModule,
+    ShowcaseTableEventsModule
   ],
   exports: [
     PanelDemoComponent

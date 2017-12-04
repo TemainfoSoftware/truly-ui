05250ac0ed39ced1d856e950e66505b6bc03abe9
William Aguera <williamaguera.m@hotmail.com>
2017-12-04 11:15:47 -0200

    docs(button): update documentation.

--- src/app/showcase/components/button/buttondemo-dataproperties.json.ts
+++ src/app/showcase/components/button/buttondemo-dataproperties.json.ts
@@ -21,13 +21,6 @@
  */
 export const dataProperties = [
   {
-    name: 'type',
-    type: 'string',
-    default: 'button',
-    description: 'Type of button.',
-    options: 'button | submit | reset'
-  },
-  {
     name: 'text',
     type: 'string',
     default: 'null',
@@ -36,17 +29,17 @@ export const dataProperties = [
   },
   {
     name: 'width',
-    type: 'number',
+    type: 'string',
     default: '125px',
     description: 'Button width.',
-    options: 'number | ex: 150'
+    options: 'px | % | em'
   },
   {
     name: 'height',
-    type: 'number',
+    type: 'string',
     default: '39',
     description: 'Button height.',
-    options: 'number | ex: 25'
+    options: 'px | % | em'
   },
   {
     name: 'disabled',

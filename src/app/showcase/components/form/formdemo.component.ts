7eb808d3e305612c4c8540916300440babf4f9a6
William Aguera <williamaguera.m@hotmail.com>
2017-12-07 14:23:49 -0200

    docs(form): update documentation.

--- src/app/showcase/components/form/formdemo.component.ts
+++ src/app/showcase/components/form/formdemo.component.ts
@@ -6,10 +6,7 @@ import { NewPersonComponent } from './newperson/newperson.component';
 import { DataFormService } from './newperson/dataform.service';
 import { DumpDataService } from '../../shared/services/dumpdata';
 import { slideToLeft } from '../../shared/animations/router.animations';
-import { ModalOptions } from '../../../components/modal';
 import { FormService } from '../../../components/form';
-
-
 @Component( {
   selector: 'app-modal',
   templateUrl: './formdemo.component.html',
@@ -22,12 +19,6 @@ export class FormDemoComponent {
 
   public index: number;
 
-  public formOptions1: ModalOptions;
-
-  public formOptions2: ModalOptions;
-
-  public formOptions3: ModalOptions;
-
   public formprop;
 
   public result;
@@ -36,70 +27,16 @@ export class FormDemoComponent {
 
   constructor(public view: ViewContainerRef, public formService: FormService,
               public dataFormService: DataFormService,  public dataDumpService: DumpDataService) {
-
     this.formService.setView(view);
     this.data = this.dataDumpService.createRandomData( 100 );
-
     this.formprop = json.dataProperties;
-
-    this.formOptions1 = {
-      title: 'User Register',
-      icon: 'ion-person-add',
-      draggable: true,
-      width: '500px',
-      height: 'auto',
-      maximizable: true,
-      minimizable: true,
-      fullscreen: false
-    };
-
-    this.formOptions2 = {
-      title: 'Pacient Register',
-      icon: 'ion-heart',
-      draggable: true,
-      width: '500px',
-      height: 'auto',
-      color: '#f27a4f',
-      maximizable: true,
-      minimizable: true,
-      fullscreen: false
-    };
-
-    this.formOptions3 = {
-      title: 'Count Register',
-      icon: 'ion-stats-bars',
-      draggable: true,
-      width: '500px',
-      color: '#5bbcf2',
-      height: 'auto',
-      maximizable: true,
-      minimizable: true,
-      fullscreen: false
-    };
   }
 
-
-  form1() {
-    this.formService.createForm(NewPersonComponent, this.formOptions1, (modalResult) => {
+  form1(parent) {
+    this.formService.createForm(NewPersonComponent, parent, (modalResult) => {
       this.dataFormService.saveDataForm(modalResult.formResult);
       this.result = this.dataFormService.getDataForm();
     });
   }
-
-  form2() {
-    this.formService.createForm(NewPersonComponent, this.formOptions2, (modalResult) => {
-      this.dataFormService.saveDataForm(modalResult.formResult);
-      this.result = this.dataFormService.getDataForm();
-    });
-  }
-
-  form3() {
-    this.formService.createForm(NewPersonComponent, this.formOptions3, (modalResult) => {
-      this.dataFormService.saveDataForm(modalResult.formResult);
-      this.result = this.dataFormService.getDataForm();
-    });
-  }
-
-
 
 }

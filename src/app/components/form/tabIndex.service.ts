2d3ccf8022eeac7afdf424dfc7c7edd6d259f305
Maicon Wagner dos Santos <mister-x59@hotmail.com.br>
2018-02-09 09:18:25 -0200

    docs(*): updated company name and year in the license headers

--- src/app/components/form/tabIndex.service.ts
+++ src/app/components/form/tabIndex.service.ts
@@ -1,7 +1,7 @@
 /*
  MIT License
 
- Copyright (c) 2018 Temainfo Sistemas
+ Copyright (c) 2018 Temainfo Software
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal

00f43f685329afbf339dca17262a6d43083224ac
William Aguera <williamaguera.m@hotmail.com>
2018-03-06 16:48:11 -0300

    refactor(chalist): refactor chatlist functions, remove functions not used and map properties for template.

--- src/app/components/chatlist/chatlist.ts
+++ src/app/components/chatlist/chatlist.ts
@@ -20,15 +20,11 @@
  SOFTWARE.
  */
 import {
-  Component, ElementRef, AfterViewInit, Input, OnInit, QueryList,
+  Component, AfterViewInit, Input, OnInit, QueryList,
   ViewChildren, Output, EventEmitter, forwardRef,
-  ViewChild, KeyValueDiffers, DoCheck, Renderer2,
+  Renderer2, ChangeDetectorRef, ChangeDetectionStrategy,
 } from '@angular/core';
 
-import { TabIndexService } from '../form/tabIndex.service';
-import { IdGeneratorService } from '../core/helper/idgenerator.service';
-import { NameGeneratorService } from '../core/helper/namegenerator.service';
-import { ComponentDefaultBase } from '../core/base/component-default.base';
 import { TlListBox } from '../listbox/listbox';
 import { ChatListStatus } from './chatlist-status';
 import { ChatListService } from './chatlist.service';
@@ -37,17 +33,26 @@ import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/debounceTime';
 
 @Component( {
-    selector: 'tl-chatlist',
-    templateUrl: './chatlist.html',
-    styleUrls: [ './chatlist.scss' ]
+  selector: 'tl-chatlist',
+  templateUrl: './chatlist.html',
+  styleUrls: [ './chatlist.scss' ],
+  changeDetection: ChangeDetectionStrategy.OnPush
 } )
-export class TlChatList extends ComponentDefaultBase implements AfterViewInit, OnInit, DoCheck {
+export class TlChatList implements AfterViewInit, OnInit {
 
   @Input() data = [];
 
   @Input() searchInput;
 
   @Input() searchQuery;
+
+  @Input() height = 300;
+
+  @Input() label = 'firstName';
+
+  @Input() labelDetail = 'lastName';
+
+  @Input() avatar = '';
 
   @Input() itemsToShow = 5;
 
@@ -57,137 +62,117 @@ export class TlChatList extends ComponentDefaultBase implements AfterViewInit, O
 
   @ViewChildren( forwardRef( () => TlListBox ) ) listBoxes: QueryList<TlListBox>;
 
-  @ViewChild( 'overflow' ) overflow: ElementRef;
-
-  public differ;
-
-  public scrollChat;
-
-  public filteredData = [];
-
   public filtering = false;
+
+  public filteredOnline = [];
+
+  public filteredOffline = [];
 
   private subject = new Subject();
 
   private selected = '';
 
-  constructor( tabIndexService: TabIndexService, idService: IdGeneratorService, nameService: NameGeneratorService,
-               differs: KeyValueDiffers,
-               public renderer: Renderer2,
-               public chatListService: ChatListService, private chat: ElementRef ) {
-    super( tabIndexService, idService, nameService );
-    this.differ = differs.find( {} ).create();
+  constructor( public renderer: Renderer2, private change: ChangeDetectorRef,
+               public chatListService: ChatListService ) {
   }
 
   ngOnInit() {
-      this.chatListService.data = this.data;
-      this.filterDataStatus(this.chatListService.data);
+    this.chatListService.data = this.data;
+    this.filterDataStatus( this.chatListService.data );
+    this.handleStatusConfig();
+    this.handleSearchInput();
+    this.listenInput();
   }
 
   ngAfterViewInit() {
-      this.setElement( this.chat, 'tl-chatlist' );
-      this.listenInputSearch();
-      this.subject.debounceTime( 500 ).subscribe( searchTextValue => {
-        this.handlefilterData( searchTextValue );
-      } );
+    this.subscribeStatusChange();
+    this.detectChangesOfLists();
+   }
+
+  private subscribeStatusChange() {
+    this.chatListService.subjectStatus.subscribe( ( value ) => {
+      this.change.detectChanges();
+      this.renderListBoxes();
+    } );
   }
 
-  filterDataStatus(array) {
-    this.chatListService.online = [];
-    this.chatListService.offline = [];
+  private listenInput() {
+    this.renderer.listen( this.searchInput.input.nativeElement, 'input', ( $event ) => {
+      this.subject.next( $event.target.value );
+    } );
+  }
+
+  private detectChangesOfLists() {
+    this.change.markForCheck();
+    this.listBoxes.forEach( ( item, index, array ) => {
+      item.change.markForCheck();
+    } );
+  }
+
+  private filterDataStatus( array ) {
+    this.resetServiceLists();
     array.forEach( ( value ) => {
-          this.isNotOffline( value ) ?
-              this.chatListService.online.push( value ) : this.chatListService.offline.push( value );
-      } );
+      this.isNotOffline( value ) ?
+        this.chatListService.online.push( value ) : this.chatListService.offline.push( value );
+    } );
+    this.filteredOffline = this.chatListService.offline;
+    this.filteredOnline = this.chatListService.online;
     this.chatListService.searchQuery = this.searchQuery;
+    this.sortLists();
+  }
+
+  private sortLists() {
     this.chatListService.sortArray( this.chatListService.online );
     this.chatListService.sortArray( this.chatListService.offline );
   }
 
-  listenInputSearch() {
-    this.renderer.listen(this.searchInput.input.nativeElement, 'keyup', ($event) => {
-      this.subject.next($event.target.value);
-    });
+  private resetServiceLists() {
+    this.chatListService.online = [];
+    this.chatListService.offline = [];
   }
 
-  getStatus(item) {
-    return Object.keys(this.statusConfig).find((key => this.statusConfig[key] === item));
-  }
-
-  handleScrollChat( $event ) {
-      this.setScrollChat( $event );
-      this.setScrollFirstList();
-      this.calculateScrollChat();
-  }
-
-  calculateScrollChat() {
-    if ( this.scrollChat + this.heightSecondList() >= this.heightFirstList() ) {
-      this.listBoxes.toArray()[ 1 ].itemContainer.nativeElement.scrollTop =
-        (this.scrollChat - this.heightFirstList());
+  private handleSearchInput() {
+    if ( !this.searchInput ) {
+      throw new Error( '[searchInput] property is required' );
     }
   }
 
-  handlefilterData( searchTerm ) {
-    !searchTerm ? this.handleFilterAsEmpty() : this.filterData(searchTerm);
+  private handleStatusConfig() {
+    if ( !this.statusConfig ) {
+      throw new Error( '[statusConfig] property is required' );
+    }
   }
 
-  filterData(searchTerm) {
-    this.filtering = true;
-    this.filteredData = [];
-    this.data.forEach( ( item ) => {
-      if (item[this.searchQuery].substr(0, searchTerm.length).toLowerCase().includes(searchTerm.toLowerCase())) {
-        if (this.filteredData.indexOf(item) < 0) {
-          this.filteredData.push(item);
-        }
-      }
+  private renderListBoxes() {
+    this.listBoxes.forEach( ( item ) => {
+      item.renderPageData();
     } );
-    this.filterDataStatus(this.filteredData);
   }
 
-  handleFilterAsEmpty() {
-    this.filtering = false;
-    this.filterDataStatus(this.chatListService.data);
+  getStatus( item ) {
+    return Object.keys( this.statusConfig ).find( (key => this.statusConfig[ key ] === item) );
   }
 
-  onClickItemChat( $event ) {
-      this.clickItem.emit( $event );
-      this.selected = $event;
-      this.setInputFocus();
+  onFilterData(data, list) {
+    list === 'list1' ? this.filteredOnline = data : this.filteredOffline = data;
+    this.change.detectChanges();
   }
 
-  setInputFocus() {
-    setTimeout(() => {
-      this.searchInput.input.nativeElement.focus();
-    }, 1);
+  onClickItemChat( $event, string ) {
+    this.clickItem.emit( $event );
+    this.selected = $event;
+    this.resetSelected( string );
   }
 
-  setScrollChat( $event ) {
-      this.scrollChat = $event.target.scrollTop;
-  }
-
-  heightFirstList() {
-      return (parseInt( this.listBoxes.toArray()[ 0 ].listBox.nativeElement.style.height, 10 ));
-  }
-
-  heightSecondList() {
-      return (parseInt( this.listBoxes.toArray()[ 1 ].itemContainer.nativeElement.style.height, 10 ));
-  }
-
-  setScrollFirstList() {
-      this.listBoxes.toArray()[ 0 ].itemContainer.nativeElement.scrollTop = this.scrollChat;
-  }
-
-  isNotOffline( value ) {
-      return value.status.toLowerCase() !== this.statusConfig['offline'].toLowerCase();
-  }
-
-  ngDoCheck() {
-    const online = this.differ.diff( this.chatListService.online );
-    if ( online && this.listBoxes ) {
-        this.listBoxes.forEach((item) => {
-          item.renderPageData();
-        });
+  private resetSelected( string ) {
+    if ( string === 'list1' ) {
+      return this.listBoxes.toArray()[ 1 ].removeSelected();
     }
+    this.listBoxes.toArray()[ 0 ].removeSelected();
+  }
+
+  private isNotOffline( value ) {
+    return value.status.toLowerCase() !== this.statusConfig[ 'offline' ].toLowerCase();
   }
 
 }

02f1212d7f6a2fcfcf88ff9c95775bd725f8b296
William Aguera <williamaguera.m@hotmail.com>
2018-03-19 18:03:38 -0300

    feat(progressbar): create a progress-bar component.

---
+++ src/app/components/progressbar/index.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TlProgressBar } from './progressbar';
+
+export * from './progressbar';
+
+@NgModule( {
+  imports: [
+    CommonModule,
+  ],
+  declarations: [
+    TlProgressBar,
+  ],
+  exports: [
+    TlProgressBar,
+  ],
+} )
+export class ProgressBarModule {
+}

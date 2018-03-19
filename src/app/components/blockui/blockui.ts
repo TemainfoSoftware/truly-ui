e95e38bd2d2210e98b978cf5b70e5ae23cae0d31
William Aguera <williamaguera.m@hotmail.com>
2018-03-19 14:07:12 -0300

    refactor(*): rename blockui on datatable and listbox.

--- src/app/components/blockui/blockui.ts
+++ src/app/components/blockui/blockui.ts
@@ -1,110 +1,119 @@
 /*
-    MIT License
+ MIT License
 
-    Copyright (c) 2018 Temainfo Software
+ Copyright (c) 2018 Temainfo Software
 
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
-*/
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
 import {
-    AfterViewInit,
-    ComponentFactoryResolver,
-    ComponentRef,
-    Directive,
-    ElementRef,
-    Input,
-    OnChanges,
-    Renderer2,
-    ViewContainerRef
+  AfterContentInit,
+  AfterViewInit, ChangeDetectorRef,
+  ComponentFactoryResolver,
+  ComponentRef,
+  Directive,
+  ElementRef,
+  Input,
+  OnChanges, OnInit,
+  Renderer2,
+  ViewContainerRef
 } from '@angular/core';
 import { BlockUIConfig } from './blockui-config';
 import { TlBlockUIComponent } from './blockui.component';
 
-@Directive({
-    selector: '[tlOverlay]'
-})
-export class TlBlockUI implements OnChanges, AfterViewInit {
+@Directive( {
+  selector: '[tlBlockui]'
+} )
+export class TlBlockUI implements OnChanges, AfterContentInit {
 
-    @Input() tlOverlay: boolean;
+  @Input() tlBlockui: boolean;
 
-    @Input() overlayConfig: BlockUIConfig = new BlockUIConfig();
+  @Input() blockuiConfig: BlockUIConfig = new BlockUIConfig();
 
-    private overlayElement: ElementRef;
+  private overlayElement: ElementRef;
 
-    private overlayElementInstance: ComponentRef<TlBlockUIComponent>;
+  private overlayElementInstance: ComponentRef<TlBlockUIComponent>;
 
-    constructor( private elementRef: ElementRef,
-                 private viewContainerRef: ViewContainerRef,
-                 private renderer: Renderer2,
-                 private compiler: ComponentFactoryResolver ) {}
+  constructor( private elementRef: ElementRef,
+               private viewContainerRef: ViewContainerRef,
+               private renderer: Renderer2,
+               private change: ChangeDetectorRef,
+               private compiler: ComponentFactoryResolver ) {
+  }
 
-    ngOnChanges(changes) {
-        if (changes['tlOverlay'] && (!changes['tlOverlay'].firstChange) ) {
-            this.toggleLoader( changes['tlOverlay'].currentValue );
-        }
+  ngOnChanges( changes ) {
+    if ( changes[ 'tlBlockui' ] && (!changes[ 'tlBlockui' ].firstChange) ) {
+      this.toggleLoader( changes[ 'tlBlockui' ].currentValue );
     }
+  }
 
-    ngAfterViewInit() {
-        this.createElementInstance();
-        this.getElementRefFromInstance();
-        this.setConfigToElement();
-        this.buildLoadingElement();
-    }
+  ngAfterContentInit() {
+    this.createElementInstance();
+    this.getElementRefFromInstance();
+    this.setConfigToElement();
+    this.buildLoadingElement();
+    this.toggleLoader( this.tlBlockui );
+  }
 
-    private createElementInstance() {
-        const componentFactory = this.compiler.resolveComponentFactory( TlBlockUIComponent );
-        this.overlayElementInstance = this.viewContainerRef.createComponent( componentFactory );
-    }
+  private createElementInstance() {
+    const componentFactory = this.compiler.resolveComponentFactory( TlBlockUIComponent );
+    this.overlayElementInstance = this.viewContainerRef.createComponent( componentFactory );
+  }
 
-    private setConfigToElement() {
-        this.overlayElementInstance.instance.config = this.overlayConfig;
-    }
+  private setConfigToElement() {
+    this.overlayElementInstance.instance.config = this.blockuiConfig;
+  }
 
-    private toggleLoader( showLoading: boolean) {
-        showLoading ? this.show() : this.hide();
-    }
+  private toggleLoader( showLoading: boolean ) {
+    showLoading ? this.show() : this.hide();
+  }
 
-    private show() {
-        if ( this.happenedResize() ) {
-           this.buildLoadingElement();
-        }
-        this.renderer.setStyle(this.elementRef.nativeElement, 'filter', 'blur(1px)');
-        this.renderer.setStyle(this.overlayElement.nativeElement, 'top', this.elementRef.nativeElement.offsetTop + 'px');
-        this.renderer.setStyle(this.overlayElement.nativeElement, 'display', 'table');
+  private show() {
+    if ( this.happenedResize() ) {
+      this.buildLoadingElement();
     }
+    this.renderer.setStyle( this.elementRef.nativeElement, 'filter', 'blur(1px)' );
+    this.renderer.setStyle( this.overlayElement.nativeElement, 'top', this.elementRef.nativeElement.offsetTop + 'px' );
+    this.renderer.setStyle( this.overlayElement.nativeElement, 'display', 'table' );
+  }
 
-    private getElementRefFromInstance() {
-        this.overlayElement = this.overlayElementInstance.instance.element;
-    }
+  private getElementRefFromInstance() {
+    this.overlayElement = this.overlayElementInstance.instance.element;
+  }
 
-    private hide() {
-        this.renderer.setStyle(this.elementRef.nativeElement, 'filter', 'blur(0px)');
-        this.renderer.setStyle(this.overlayElement.nativeElement, 'display', 'none');
-    }
+  private hide() {
+    this.renderer.setStyle( this.elementRef.nativeElement, 'filter', 'blur(0px)' );
+    this.renderer.setStyle( this.overlayElement.nativeElement, 'display', 'none' );
+  }
 
-    private happenedResize() {
-        if (this.overlayElement.nativeElement.style.height !== this.elementRef.nativeElement.clientHeight + 'px') { return true; }
-        if (this.overlayElement.nativeElement.style.width !== this.elementRef.nativeElement.clientWidth + 'px') { return true; }
+  private happenedResize() {
+    if ( this.overlayElement.nativeElement.style.height !== this.elementRef.nativeElement.clientHeight + 'px' ) {
+      return true;
     }
+    if ( this.overlayElement.nativeElement.style.width !== this.elementRef.nativeElement.clientWidth + 'px' ) {
+      return true;
+    }
+  }
 
-   private buildLoadingElement() {
-        this.renderer.setStyle(this.overlayElement.nativeElement, 'height', this.elementRef.nativeElement.clientHeight + 'px');
-        this.renderer.setStyle(this.overlayElement.nativeElement, 'width', this.elementRef.nativeElement.clientWidth + 'px');
-        this.renderer.setStyle(this.overlayElement.nativeElement, 'position', 'absolute');
-        this.renderer.setStyle(this.overlayElement.nativeElement, 'display', 'none');
-        this.renderer.setStyle(this.overlayElement.nativeElement, 'background-color', 'rgba(245, 245, 245, 0.8)');
-    }
+  private buildLoadingElement() {
+    this.renderer.setStyle( this.overlayElement.nativeElement, 'height', this.elementRef.nativeElement.clientHeight + 'px' );
+    this.renderer.setStyle( this.overlayElement.nativeElement, 'width', this.elementRef.nativeElement.clientWidth + 'px' );
+    this.renderer.setStyle( this.overlayElement.nativeElement, 'position', 'absolute' );
+    this.renderer.setStyle( this.overlayElement.nativeElement, 'display', 'none' );
+    this.renderer.setStyle( this.overlayElement.nativeElement, 'background-color', 'rgba(245, 245, 245, 0.8)' );
+    this.change.detectChanges();
+  }
 }

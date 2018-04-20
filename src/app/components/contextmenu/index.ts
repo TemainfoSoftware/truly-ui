836a839d60b7bc7f43dd689c8c33dffabc87f9f6
William Aguera <williamaguera.m@hotmail.com>
2018-04-20 14:21:18 -0300

    refactor(context-menu): change operation mode to use sub menu service.

--- src/app/components/contextmenu/index.ts
+++ src/app/components/contextmenu/index.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { TlContextMenu } from './context-menu';
 import { PopupMenuModule } from '../popupmenu/index';
+import { TlSimpleSubMenu } from '../menu/parts/simple/simple-sub-menu';
 
 export * from './context-menu';
 
@@ -17,6 +18,9 @@ export * from './context-menu';
   exports: [
     TlContextMenu,
   ],
+  entryComponents: [
+    TlSimpleSubMenu
+  ]
 } )
 export class ContextMenuModule {
 }

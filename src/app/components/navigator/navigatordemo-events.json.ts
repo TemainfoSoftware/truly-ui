7cd732d1c2fc710416f1cd51e60b4015c68da58e
Maicon Wagner dos Santos <mister-x59@hotmail.com.br>
2018-05-28 17:10:47 -0300

    doc(navigator): add events clickprevious and clicknext to navigator

--- src/app/components/navigator/navigatordemo-events.json.ts
+++ src/app/components/navigator/navigatordemo-events.json.ts
@@ -1,7 +1,13 @@
 export const dataEvents = [
   {
     name: 'clickNavigator',
-    parameters: [],
+    parameters: [
+      { event: 'event.year', description: 'Returning year in navigation.' },
+      { event: 'event.day', description: 'Returning day in navigation.' },
+      { event: 'event.month', description: 'Returning month in navigation.' },
+      { event: 'event.fullDate', description: 'Returning default format date.' },
+      { event: 'event.rangeYear', description: 'Start year and dnd year selected in range.' }
+    ],
     description: 'Callback triggered by clicking Navigator Button.'
   },
   {

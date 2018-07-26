a2ff7007e66ce6b8e238d3e369f6dc0966b52129
William Aguera <williamaguera.m@hotmail.com>
2018-07-26 15:50:25 -0300

    docs(dropdownicon): create dropdownicon docs page.

---
+++ src/app/components/dropdownicon/dropdownicondemo.module.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+import { HighlightJsModule } from 'ngx-highlight-js';
+
+import { DropDownIconDemoComponent } from './dropdownicondemo.component';
+import { DropDownIconDemoRoutingModule } from './dropdownicondemo-routing.module';
+import { ShowcaseCardModule } from '../../shared/components/showcase-card/showcase-card.module';
+import { ShowcaseTableEventsModule } from '../../shared/components/showcase-table-events/showcase-table-events.module';
+import { ShowcaseTablePropertiesModule } from '../../shared/components/showcase-table-properties/showcase-table-properties.module';
+import { ShowcaseReturnedValueModule } from '../../shared/components/showcase-returned-value/showcase-returned-value.module';
+import { DropDownIconModule } from '../../../../projects/truly-ui/src/components/dropdownicon/index';
+
+@NgModule({
+  imports: [
+    DropDownIconDemoRoutingModule,
+    DropDownIconModule,
+    CommonModule,
+    FormsModule,
+    HighlightJsModule,
+    ShowcaseCardModule,
+    ShowcaseTableEventsModule,
+    ShowcaseTablePropertiesModule,
+    ShowcaseReturnedValueModule
+  ],
+  declarations: [
+    DropDownIconDemoComponent
+  ],
+  exports: [
+    DropDownIconDemoComponent
+  ]
+})
+export class DropDownIconDemoModule { }

c4f1ec7f2c684618142d14ecb57c33bbfed89d0c
William Aguera <williamaguera.m@hotmail.com>
2018-09-17 17:47:23 -0300

    refactor(accordion): refactor accordion style and animation.

--- projects/truly-ui/src/components/accordion/parts/accordion-item/accordion-item.ts
+++ projects/truly-ui/src/components/accordion/parts/accordion-item/accordion-item.ts
@@ -1,25 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { AccordionService } from '../../services/accordion.service';
-import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'tl-accordion-item',
   templateUrl: './accordion-item.html',
   styleUrls: ['./accordion-item.scss'],
-  animations: [
-    trigger(
-      'enterAnimation', [
-        transition(':enter', [
-          style({opacity: 0}),
-          animate('500ms', style({ opacity: 1}))
-        ]),
-        transition(':leave', [
-          style({opacity: 1}),
-          animate('300ms', style({opacity: 0}))
-        ])
-      ]
-    )
-  ],
 })
 export class TlAccordionItem  {
 

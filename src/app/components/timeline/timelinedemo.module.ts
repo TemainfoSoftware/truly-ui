a3783374068a5639d1ea5087e1c0d643ae5a0078
Genesson <genesson_sauer@hotmail.com>
2018-09-18 15:24:41 -0300

    doc(timeline): create module an component timeline

---
+++ src/app/components/timeline/timelinedemo.module.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+import { HighlightJsModule } from 'ngx-highlight-js';
+
+import { TimelineDemoRoutingModule } from './timelinedemo-routing.module';
+import { TimelineDemoComponent } from './timelinedemo.component';
+import { TimelineModule } from '../../../../projects/truly-ui/src/components/timeline';
+import { ShowcaseCardModule } from '../../shared/components/showcase-card/showcase-card.module';
+import { ShowcaseTablePropertiesModule } from '../../shared/components/showcase-table-properties/showcase-table-properties.module';
+import { ShowcaseTableEventsModule } from '../../shared/components/showcase-table-events/showcase-table-events.module';
+
+@NgModule({
+  declarations: [
+    TimelineDemoComponent
+  ],
+  imports: [
+    TimelineDemoRoutingModule,
+    TimelineModule,
+    CommonModule,
+    FormsModule,
+    HighlightJsModule,
+    ShowcaseCardModule,
+    ShowcaseTablePropertiesModule,
+    ShowcaseTableEventsModule
+  ],
+  exports: [
+    TimelineDemoComponent
+  ]
+})
+export class TimelineDemoModule {}

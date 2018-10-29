837994c6f14cfd6302fe8cb614d97c1aa0ec320b
William Aguera <williamaguera.m@hotmail.com>
2018-10-29 10:51:55 -0200

    refact(overlaylist.module): remove list option directive.

--- projects/truly-ui/src/components/overlaylist/index.ts
+++ projects/truly-ui/src/components/overlaylist/index.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { InputModule } from '../input/index';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { A11yModule } from '@angular/cdk/a11y';
-import { ListOptionDirective } from './directives/listoption.directive';
 import { TlOverlayList } from './overlay-list';
 import { MiscModule } from '../misc/index';
 import { TlListItem } from './list-item/list-item';
@@ -22,12 +21,10 @@ import { TlListItem } from './list-item/list-item';
   declarations: [
     TlOverlayList,
     TlListItem,
-    ListOptionDirective,
   ],
   exports: [
     TlOverlayList,
     TlListItem,
-    ListOptionDirective,
   ],
 } )
 export class OverlayListModule {

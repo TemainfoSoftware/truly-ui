e4ad1ce603d5c4169405a0e2cb887c90968839d7
Genesson <genesson_sauer@hotmail.com>
2018-11-06 15:07:54 -0200

    fix(icons): delete line of import incorret

--- projects/truly-ui/src/components/icons/index.ts
+++ projects/truly-ui/src/components/icons/index.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import {  } from ''
-
 import { TlIcons } from './icons';
 
 @NgModule({

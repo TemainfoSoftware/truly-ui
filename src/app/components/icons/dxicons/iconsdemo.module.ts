c9bb593556ed8b3312f6219734a93134ba250a4a
Genesson <genesson_sauer@hotmail.com>
2018-11-08 10:42:34 -0200

    doc(icons): create icons examples

--- src/app/components/icons/dxicons/iconsdemo.module.ts
+++ src/app/components/icons/dxicons/iconsdemo.module.ts
@@ -6,10 +6,10 @@ import { HighlightJsModule } from 'ngx-highlight-js';
 
 import { IconsDemoRoutingModule } from './iconsdemo-routing.module';
 import { IconsDemoComponent } from './iconsdemo.component';
-import { IconsModule } from '../../../../projects/truly-ui/src/components/icons';
-import { ShowcaseCardModule } from '../../shared/components/showcase-card/showcase-card.module';
-import { ShowcaseTablePropertiesModule } from '../../shared/components/showcase-table-properties/showcase-table-properties.module';
-import { ShowcaseTableEventsModule } from '../../shared/components/showcase-table-events/showcase-table-events.module';
+import { IconsModule } from '../../../../../projects/truly-ui/src/components/icons';
+import { ShowcaseCardModule } from '../../../shared/components/showcase-card/showcase-card.module';
+import { ShowcaseTablePropertiesModule } from '../../../shared/components/showcase-table-properties/showcase-table-properties.module';
+import { ShowcaseTableEventsModule } from '../../../shared/components/showcase-table-events/showcase-table-events.module';
 
 @NgModule({
   declarations: [

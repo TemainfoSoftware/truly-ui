09eeb05ddd7f343bbad32bda04c40b3cc6e792b2
William Aguera <williamaguera.m@hotmail.com>
2018-11-13 10:17:58 -0200

    feat(timepicker): create timepicker component

---
+++ projects/truly-ui/src/components/timepicker/index.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { TlTimepicker } from './timepicker';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { InputModule } from '../input/index';
+import { ButtonModule } from '../button/index';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    OverlayModule,
+    InputModule,
+    ButtonModule,
+    FormsModule
+  ],
+  declarations: [
+    TlTimepicker
+  ],
+  exports: [
+    TlTimepicker
+  ]
+})
+export class TimepickerModule {}

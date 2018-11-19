415640641c62dc0fec23a984725af678ba27f3c5
Genesson <genesson_sauer@hotmail.com>
2018-11-19 15:43:20 -0200

    feat(input): use new component to display the icons

--- projects/truly-ui/src/components/input/index.ts
+++ projects/truly-ui/src/components/input/index.ts
@@ -8,6 +8,7 @@ import { CharcaseDirective } from './directives/charcase.directive';
 
 import { ValidatorsModule } from '../validators/index';
 import { OverlayModule } from '@angular/cdk/overlay';
+import { IconsModule } from '../icons/index';
 import { InternalsModule } from '../internals/index';
 import { CurrencyDirective } from './directives/currency/currency.directive';
 import { CURRENCY_MASK_CONFIG, CurrencyConfig } from './directives/currency/currency-mask.config';
@@ -17,6 +18,7 @@ import { CURRENCY_MASK_CONFIG, CurrencyConfig } from './directives/currency/curr
     CommonModule,
     FormsModule,
     ValidatorsModule,
+    IconsModule,
     InternalsModule,
     OverlayModule
   ],

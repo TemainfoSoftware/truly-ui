54670ca6d38ecf879ea7dd731977c4cf2b3838cf
Genesson <genesson_sauer@hotmail.com>
2018-11-21 09:43:34 -0200

    fix(overlaylist): change icons

--- projects/truly-ui/src/components/overlaylist/index.ts
+++ projects/truly-ui/src/components/overlaylist/index.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { IconsModule } from '../icons/index';
 import { InputModule } from '../input/index';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { A11yModule } from '@angular/cdk/a11y';
@@ -13,6 +14,7 @@ import { TlListItem } from './list-item/list-item';
   imports: [
     MiscModule,
     CommonModule,
+    IconsModule,
     InputModule,
     OverlayModule,
     A11yModule,

89fdaeb5dede5ac6b6fe35b6b33c6f13aebe24f1
Maicon Wagner <mister-x59@hotmail.com.br>
2019-02-27 14:32:07 -0300

    ci: fix ci npm ERR! code E404 npm ERR! 404 Not Found: 0.0.0-dev-develop-dc06503@latest #5

--- tools/scripts/publish-dev-builds.js
+++ tools/scripts/publish-dev-builds.js
@@ -1,8 +1,10 @@
 const { parse } = require('semver');
 const { execute, publishPackagesToNpm } = require('./utils');
+const { resolve } = require('path');
 
 async function main() {
   const json = require('../../dist/package.json');
+  const buildPath = resolve(__dirname, '../../dist/');
   let commit = process.env.TRAVIS_PULL_REQUEST_SHA;
   if (!commit) {
     const lastCommit = await execute('git rev-parse HEAD');
@@ -12,10 +14,13 @@ async function main() {
   // shorten commit
   commit = commit.slice(0, 7);
 
-  await execute(`npm view ${json.name} version`).then( async ( version )=> {
-    const newVersion = (version + '-develop-' + commit);
-    console.log('publishing new version', newVersion);
-    await publishPackagesToNpm(newVersion, 'dev');
+  await execute(`npm view ${json.name} version`).then( async ( version ) => {
+    console.log( buildPath);
+    await execute(`npm version ${version}`, { cwd: buildPath }).then( async () => {
+      const newVersion = (version + '-develop-' + commit);
+      console.log('publishing new version', newVersion);
+      await publishPackagesToNpm(newVersion, 'dev');
+    });
   });
 
 }

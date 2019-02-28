f4ba14e8d1adbec5709fef561d06aaa07d8ba868
William Aguera <williamaguera.m@hotmail.com>
2019-02-28 16:25:39 -0300

    docs(dropdownlist): update documentation.

--- src/app/components/dropdownlist/dropdownlistdemo.component.ts
+++ src/app/components/dropdownlist/dropdownlistdemo.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 import * as json from './dropdownlistdemo-dataproperties.json';
 
 @Component( {
-  selector : 'app-dropdown-list',
-  templateUrl : './dropdownlistdemo.component.html',
-  styleUrls : [ './dropdownlistdemo.component.scss' ]
+  selector: 'app-dropdown-list',
+  templateUrl: './dropdownlistdemo.component.html',
+  styleUrls: [ './dropdownlistdemo.component.scss' ]
 } )
 export class DropDownListDemoComponent {
 
@@ -29,50 +29,56 @@ export class DropDownListDemoComponent {
 
   public itemSelected9: any;
 
-  public data: any[];
+  public data = [];
 
-  public bigData: any[];
+  public bigData = [];
 
-  public dataWidthIcon: any[];
+  public groupData = [];
 
-  public simpleData: any[];
+  public dataWithIcon = [];
 
   constructor() {
     this.dataTableProperties = json.dataProperties;
     this.data = [
-      { textItem : 'Wallace', value : '1' },
-      { textItem : 'Wilson', value : '2' },
-      { textItem : 'Wanda', value : '3' },
-      { textItem : 'Wanderson', value : '4' },
-      { textItem : 'Wanderlei', value : '5' }
+      { textItem: 'Wallace', value: '1' },
+      { textItem: 'Wilson', value: '2' },
+      { textItem: 'Wanda', value: '3' },
+      { textItem: 'Wanderlei', value: '5' }
     ];
     this.bigData = [
-      { textItem : 'Item 1', value : '1' },
-      { textItem : 'Item 2', value : '2' },
-      { textItem : 'Item 3', value : '3' },
-      { textItem : 'Item 4', value : '4' },
-      { textItem : 'Item 5', value : '5' },
-      { textItem : 'Item 6', value : '6' },
-      { textItem : 'Item 7', value : '7' },
-      { textItem : 'Item 8', value : '8' },
-      { textItem : 'Item 9', value : '9' },
-      { textItem : 'Item 10', value : '10' },
-      { textItem : 'Item 11', value : '11' },
-      { textItem : 'Item 12', value : '12' },
-      { textItem : 'Item 13', value : '13' },
-      { textItem : 'Item 14', value : '14' },
-      { textItem : 'Item 15', value : '15' },
-      { textItem : 'Item 16', value : '16' }
+      { textItem: 'Item 1', value: '1' },
+      { textItem: 'Item 2', value: '2' },
+      { textItem: 'Item 3', value: '3' },
+      { textItem: 'Item 4', value: '4' },
+      { textItem: 'Item 5', value: '5' },
+      { textItem: 'Item 6', value: '6' },
+      { textItem: 'Item 7', value: '7' },
+      { textItem: 'Item 8', value: '8' },
+      { textItem: 'Item 9', value: '9' },
+      { textItem: 'Item 10', value: '10' },
+      { textItem: 'Item 11', value: '11' },
+      { textItem: 'Item 12', value: '12' },
+      { textItem: 'Item 13', value: '13' },
+      { textItem: 'Item 14', value: '14' },
+      { textItem: 'Item 15', value: '15' },
+      { textItem: 'Item 16', value: '16' }
     ];
-    this.dataWidthIcon = [
-      { textItem : 'Contains', value : '1', icon: 'fa fa-arrows' },
-      { textItem : 'Does not Contains', value : '2', icon: 'fa fa-arrows-v' },
-      { textItem : 'Ends Withs', value : '3', icon: 'fa fa-bus' },
-      { textItem : 'Equals', value : '4', icon: 'fa fa-circle' },
-      { textItem : 'Not Equals', value : '5', icon: 'fa fa-external-link' },
-      { textItem : 'Reset', value : '6', icon: 'ion-help-buoy' }
+    this.groupData = [
+      { textItem: 'Aline', value: '1', gender: 'Female' },
+      { textItem: 'Anne', value: '2',  gender: 'Female' },
+      { textItem: 'Laura', value: '3',  gender: 'Female' },
+      { textItem: 'Wilson', value: '4',  gender: 'Male' },
+      { textItem: 'Wanda', value: '5',  gender: 'Female' },
+      { textItem: 'Wanderlei', value: '6',  gender: 'Male' }
     ];
-    this.simpleData = [ 'Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5' ];
+    this.dataWithIcon = [
+      { textItem: 'Contains', value: '1', icon: 'fa fa-arrows' },
+      { textItem: 'Does not Contains', value: '2', icon: 'fa fa-arrows-v' },
+      { textItem: 'Ends Withs', value: '3', icon: 'fa fa-bus' },
+      { textItem: 'Equals', value: '4', icon: 'fa fa-circle' },
+      { textItem: 'Not Equals', value: '5', icon: 'fa fa-external-link' },
+      { textItem: 'Reset', value: '6', icon: 'ion-help-buoy' }
+    ];
   }
 
 }

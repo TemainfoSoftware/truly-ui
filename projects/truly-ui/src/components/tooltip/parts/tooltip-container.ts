9f0bf08d929675434861825e35027fe89319e8cc
William Aguera <williamaguera.m@hotmail.com>
2019-03-08 09:42:43 -0300

    refact(tooltip): refact tooltip to use cdk overlay positioning.

---
+++ projects/truly-ui/src/components/tooltip/parts/tooltip-container.ts
@@ -0,0 +1,130 @@
+import { AfterContentInit, AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { transition, trigger, style, animate } from '@angular/animations';
+import { CdkConnectedOverlay, CdkOverlayOrigin } from '@angular/cdk/overlay';
+import { TooltipOptions } from '../tooltipOptions';
+import { OverlayAnimation } from '../../core/directives/overlay-animation';
+
+@Component( {
+  selector: 'tl-tooltip-container',
+  template: `
+    <ng-template cdkConnectedOverlay
+                 [cdkConnectedOverlayHasBackdrop]="false"
+                 [cdkConnectedOverlayBackdropClass]="'cdk-overlay-transparent-backdrop'"
+                 [cdkConnectedOverlayOpen]="true">
+      <tl-tooltip @overlay [options]="options"></tl-tooltip>
+    </ng-template>
+  `,
+  animations: [OverlayAnimation],
+} )
+export class TlToolTipContainer implements AfterContentInit {
+
+  @ViewChild( 'tooltip' ) tooltip;
+
+  @ViewChild( CdkConnectedOverlay ) connectedOverlay: CdkConnectedOverlay;
+
+  public options: TooltipOptions;
+
+  private element;
+
+  setOptions( options: TooltipOptions ) {
+    this.options = options;
+  }
+
+  setElement( element ) {
+    this.element = element;
+  }
+
+  ngAfterContentInit() {
+    this.connectedOverlay.origin = new CdkOverlayOrigin( this.element );
+    this.getPositionsByPlacement();
+  }
+
+  getPositionsByPlacement() {
+    const placements = {
+      'left-top': () => this.setLeftTop(),
+      'left-center' : () => this.setLeftCenter(),
+      'left-bottom' : () => this.setLeftBottom(),
+      'right-top': () => this.setRightTop(),
+      'right-center': () => this.setRightCenter(),
+      'right-bottom': () => this.setRightBottom(),
+      'top-center': () => this.setTopCenter(),
+      'bottom-center': () => this.setBottomCenter()
+    };
+    if (placements[this.options.placement]) {
+      placements[this.options.placement]();
+    }
+  }
+
+  setBottomCenter() {
+    this.connectedOverlay.positions = [ {
+      originX: 'center',
+      originY: 'bottom',
+      overlayX: 'center',
+      overlayY: 'top'
+    } ];
+  }
+
+  setTopCenter() {
+    this.connectedOverlay.positions = [ {
+      originX: 'center',
+      originY: 'top',
+      overlayX: 'center',
+      overlayY: 'bottom'
+    } ];
+  }
+
+  setRightCenter() {
+    this.connectedOverlay.positions = [ {
+      originX: 'end',
+      originY: 'center',
+      overlayX: 'start',
+      overlayY: 'center'
+    } ];
+  }
+
+  setRightTop() {
+    this.connectedOverlay.positions = [ {
+      originX: 'end',
+      originY: 'top',
+      overlayX: 'start',
+      overlayY: 'top'
+    } ];
+  }
+
+  setRightBottom() {
+    this.connectedOverlay.positions = [ {
+      originX: 'end',
+      originY: 'bottom',
+      overlayX: 'start',
+      overlayY: 'bottom'
+    } ];
+  }
+
+  setLeftBottom() {
+    this.connectedOverlay.positions = [ {
+      originX: 'start',
+      originY: 'bottom',
+      overlayX: 'end',
+      overlayY: 'bottom'
+    } ];
+  }
+
+  setLeftTop() {
+    this.connectedOverlay.positions = [ {
+      originX: 'start',
+      originY: 'top',
+      overlayX: 'end',
+      overlayY: 'top'
+    } ];
+  }
+
+  setLeftCenter() {
+    this.connectedOverlay.positions = [ {
+      originX: 'start',
+      originY: 'center',
+      overlayX: 'end',
+      overlayY: 'center'
+    } ];
+  }
+
+}
--- projects/truly-ui/src/components/tooltip/tooltip.ts
+++ projects/truly-ui/src/components/tooltip/tooltip.ts
@@ -1,197 +1,29 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterContentInit, Input, Component, ElementRef, ViewChild } from '@angular/core';
 import { TooltipOptions } from './tooltipOptions';
-import { transition, trigger, style, animate } from '@angular/animations';
+import { OverlayAnimation } from '../core/directives/overlay-animation';
 
 @Component( {
-    selector: 'tl-tooltip',
-    template: `
-        <div #tooltip [@enterAnimation]="show" class="tooltip-text" [ngStyle]="getStyleTooltip()"><span>{{text}}</span>
-            <div [ngClass]="'-'+placement" [ngStyle]="getStyleTooltipArrow()"></div>
-        </div>`,
-    styleUrls: [ './tooltip.scss' ],
-    animations: [
-        trigger(
-            'enterAnimation', [
-                transition( ':enter', [
-                    style( { opacity: 0 } ),
-                    animate( '200ms', style( { opacity: 1 } ) )
-                ] ),
-                transition( ':leave', [
-                    style( { opacity: 1 } ),
-                    animate( '200ms', style( { opacity: 0 } ) )
-                ] )
-            ]
-        )
-    ],
+  selector: 'tl-tooltip',
+  templateUrl: './tooltip.html',
+  styleUrls: [ './tooltip.scss' ],
+  animations: [ OverlayAnimation ],
 } )
-export class TlToolTip implements TooltipOptions {
+export class TlToolTip {
 
-    @ViewChild( 'tooltip' ) tooltip;
+  @Input() options: TooltipOptions;
 
-    public text: string;
-
-    public color = '#000';
-
-    public placement = 'top';
-
-    public fontColor = '#FFF';
-
-    public width = '';
-
-    public show = true;
-
-    private tooltipWidth = 0;
-
-    private elementWidth = 0;
-
-    private elementHeight = 0;
-
-    private tooltipPadding = 10;
-
-    private element;
-
-    setOptions( options: TooltipOptions ) {
-        const self = this;
-        Object.keys( options ).forEach( function ( key ) {
-            self[ key ] = options[ key ];
-        } );
+  getClassByPlacement() {
+    if ( this.options.placement.includes( 'left' ) ) {
+      return 'left';
     }
-
-    setPlacementTop() {
-        const self = this;
-        this.getElementWidth();
-        this.getElementHeight();
-        setTimeout( function () {
-            self.getTopMeasureForTopTooltip();
-            self.setAlignCenter();
-        }, 0 );
-
+    if ( this.options.placement.includes( 'right' ) ) {
+      return 'right';
     }
-
-    setPlacementLeft() {
-        const self = this;
-        setTimeout( function () {
-            self.getTopMeasureForLeftAndRight();
-            self.setAlignLeft();
-        }, 0 );
+    if ( this.options.placement.includes( 'top' ) ) {
+      return 'top';
     }
-
-    setPlacementRight() {
-        const self = this;
-        this.getElementWidth();
-        setTimeout( function () {
-            self.getTopMeasureForLeftAndRight();
-            self.setAlignRight();
-        }, 0 );
+    if ( this.options.placement.includes( 'bottom' ) ) {
+      return 'bottom';
     }
-
-    setPlacementBottom() {
-        const self = this;
-        this.getElementWidth();
-        this.getElementHeight();
-        setTimeout( function () {
-            self.setTopMeasureForBottom();
-            self.setAlignCenter();
-        }, 0 );
-    }
-
-    getStyleTooltip() {
-        return { 'background-color': this.color, 'color': this.fontColor, 'width': this.width };
-    }
-
-    getStyleTooltipArrow() {
-        switch ( this.placement ) {
-            case 'right':
-                return {
-                    'border-color': 'transparent ' + this.color + ' transparent transparent'
-                };
-            case 'left':
-                return {
-                    'border-color': 'transparent transparent transparent' + this.color
-                };
-            case 'top':
-                return {
-                    'border-color': this.color + ' transparent transparent transparent'
-                };
-            case 'bottom':
-                return {
-                    'border-color': 'transparent transparent ' + this.color + ' transparent'
-                };
-        }
-    }
-
-    setPosition( element: ElementRef ) {
-        this.getElement( element );
-        switch ( this.placement ) {
-            case 'top':
-                this.setPlacementTop();
-                break;
-            case 'left':
-                this.setPlacementLeft();
-                break;
-            case 'right':
-                this.setPlacementRight();
-                break;
-            case 'bottom':
-                this.setPlacementBottom();
-                break;
-        }
-    }
-
-    private getElement( element ) {
-        this.element = element;
-    }
-
-    private existsMeasureOnNativeElement(): boolean {
-        return this.element.nativeElement.offsetWidth > 0 && this.element.nativeElement.offsetHeight > 0;
-    }
-
-    private isLessOrEqualThanNormalWidth(): boolean {
-        return this.tooltip.nativeElement.offsetHeight <= 27;
-    }
-
-    private getElementWidth() {
-        this.existsMeasureOnNativeElement() ? this.elementWidth = this.element.nativeElement.offsetWidth
-            : this.elementWidth = this.element.nativeElement.firstChild.clientWidth;
-    }
-
-    private getTopMeasureForTopTooltip() {
-        this.tooltip.nativeElement.style.top = this.getElementTop() -
-                this.tooltip.nativeElement.offsetHeight - this.tooltipPadding + 'px';
-    }
-
-    private getTopMeasureForLeftAndRight() {
-      this.tooltip.nativeElement.style.top =
-        this.getElementTop() + (this.element.nativeElement.offsetHeight / 2) -
-         (this.tooltip.nativeElement.offsetHeight / 2) + 'px';
-    }
-
-    private getElementHeight() {
-        this.existsMeasureOnNativeElement() ? this.elementHeight = this.element.nativeElement.offsetHeight :
-            this.elementHeight = this.element.nativeElement.firstChild.clientHeight;
-    }
-
-    private setTopMeasureForBottom() {
-        this.tooltip.nativeElement.style.top =
-          this.getElementTop() + this.elementHeight + this.tooltipPadding + 'px';
-    }
-
-    private setAlignCenter() {
-        this.tooltip.nativeElement.style.left = this.element.nativeElement.getBoundingClientRect().left +
-            (this.elementWidth / 2) - this.tooltip.nativeElement.offsetWidth / 2 + 'px';
-    }
-
-    private setAlignRight() {
-        this.tooltip.nativeElement.style.left = this.element.nativeElement.getBoundingClientRect().left +
-            this.elementWidth + this.tooltipPadding + 'px';
-    }
-
-    private setAlignLeft() {
-        this.tooltip.nativeElement.style.left = this.element.nativeElement.getBoundingClientRect().left
-          - this.tooltip.nativeElement.offsetWidth + 'px';
-    }
-
-    private getElementTop() {
-        return this.element.nativeElement.getBoundingClientRect().top;
-    }
+  }
 }

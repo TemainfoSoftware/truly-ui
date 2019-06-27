d7a0b4ec6e515a9067437f5343c44b73387eef8e
William Aguera <william@temainfo.com.br>
2019-06-27 16:28:56 -0300

    refact(panelgroup): change import of shortcut

--- projects/truly-ui/src/components/panelgroup/index.ts
+++ projects/truly-ui/src/components/panelgroup/index.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TlPanelGroup } from './panelgroup';
 import { IconsModule } from '../icons/index';
-import { ShortcutModule } from '../shortcut';
+import { ShortcutModule } from '../shortcut/index';
 
 @NgModule({
     imports: [

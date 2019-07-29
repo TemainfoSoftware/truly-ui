f57682a373a67a24ef52cb8d7651b1d04da0a763
Maicon Wagner <mister-x59@hotmail.com.br>
2019-07-29 17:58:52 -0300

    doc(button): create stories of button

--- src/stories/button/button.stories.ts
+++ src/stories/button/button.stories.ts
@@ -23,40 +23,91 @@
  */
 
 import { moduleMetadata, storiesOf } from '@storybook/angular';
-import { text, select, withKnobs } from '@storybook/addon-knobs';
+import { text, select, boolean } from '@storybook/addon-knobs';
 
 import { ButtonModule } from '../../../projects/truly-ui/src/components/button';
 import { CoreModule } from '../../../projects/truly-ui/src/components/core';
 import { TlButton } from '../../../projects/truly-ui/src/components/button/button';
-import { action } from '@storybook/addon-actions';
+import { IconsModule } from '../../../projects/truly-ui/src/components/icons';
 
 storiesOf('General|Button', module)
-  // .addParameters({
-  //   knobs: {
-  //     disableDebounce: true,
-  //   },
-  // })
-  // .addDecorator(withKnobs)
   .addDecorator(
     moduleMetadata({
       imports: [
         ButtonModule,
+        IconsModule,
         CoreModule.forRoot({
           theme: 'default',
         }),
       ],
     })
   )
-
-  .add('with text', () => ({
+  .add('Basic', () => ({
       component: TlButton,
       props: {
         text: 'Hello Button',
-        onClick: action('log 1'),
       },
     })
   )
-  .add('with color', () => {
+  .add('Colors', () => ({
+      template: `
+<div style="display: flex; width: 530px; justify-content: space-between;">
+  <tl-button [text]="'Basic'" [color]="'basic'"></tl-button>
+  <tl-button [text]="'Primary'" [color]="'primary'"></tl-button>
+  <tl-button [text]="'Success'" [color]="'success'"></tl-button>
+  <tl-button [text]="'Danger'" [color]="'danger'"></tl-button>
+  <tl-button [text]="'Warning'" [color]="'warning'"></tl-button>
+  <tl-button [text]="'Information'" [color]="'information'"></tl-button>
+</div>`,
+    })
+  )
+  .add('Only Icon', () => ({
+      template: `
+<ng-template #icon>
+  <div style="padding: 0 10px">
+    <tl-icon  [lib]="'fa'" [style]="'fas'">home</tl-icon>
+  </div>
+</ng-template>
+<div style="display: flex; width: 300px; justify-content: space-between;">
+  <tl-button width="'40px'" [template]="icon" [color]="'basic'"></tl-button>
+  <tl-button width="'40px'" [template]="icon" [color]="'primary'"></tl-button>
+  <tl-button width="'40px'" [template]="icon" [color]="'success'"></tl-button>
+  <tl-button width="'40px'" [template]="icon" [color]="'danger'"></tl-button>
+  <tl-button width="'40px'" [template]="icon" [color]="'warning'"></tl-button>
+  <tl-button width="'40px'" [template]="icon" [color]="'information'"></tl-button>
+</div>`,
+    })
+  )
+  .add(' Icon Addon', () => ({
+    template: `<p><tl-button [text]="'Icon Addon Before'" [iconAddonBefore]="'fas fa-print'" ></tl-button></p>
+               <p><tl-button [text]="'Icon Addon After'" [iconAddonAfter]="'fas fa-print'"></tl-button></p>`,
+    })
+  )
+  .add(' Icon Inside', () => ({
+      template: `<p><tl-button [text]="'Icon Before Text'" [iconBeforeText]="'fas fa-print'" ></tl-button></p>
+                 <p><tl-button [text]="'Icon After Text'" [iconAfterText]="'fas fa-print'"></tl-button></p>`,
+    })
+  )
+  .add('Dimensions', () => ({
+      template: `<p><tl-button [text]="'Width Example'" [width]="'125px'"></tl-button></p>
+                 <p><tl-button [text]="'Height Example'" [height]="'40px'" ></tl-button></p>`,
+    })
+  )
+  .add('Loading Button', () => {
+
+    const label = 'Loading ?';
+    const value = boolean(label, false);
+    return {
+      template: `<tl-button [isLoading]="isLoading"
+                 [width]="'130px'" [color]="'primary'"
+                 [text]="'Click Me'"
+                 [textLoading]="'Saving'"></tl-button>`,
+      props: {
+        isLoading: value,
+      }
+    };
+  })
+  .add('Overview', () => {
     const colors = {
       Primary: 'primary',
       Warning: 'warning',
@@ -72,4 +123,4 @@ storiesOf('General|Button', module)
         color: color,
       },
     };
-  });
+  })

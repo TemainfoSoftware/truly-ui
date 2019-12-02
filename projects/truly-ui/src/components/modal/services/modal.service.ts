61c776789710a0d0b502b5277745920bf39b4fb6
WilliamAguera <williamaguera.m@hotmail.com>
2019-12-02 17:05:05 -0300

    refact(modal.service): add message on tittle of dialog's when it's using smart form modal.

--- projects/truly-ui/src/components/modal/services/modal.service.ts
+++ projects/truly-ui/src/components/modal/services/modal.service.ts
@@ -84,7 +84,7 @@ export class ModalService implements OnDestroy {
               private containerModal: ContainerModalService) {
   }
 
-  createModalDialog(component: Type<any>, factoryResolver, mdOptions) {
+  createModalDialog(component: Type<any>, factoryResolver, mdOptions?: ModalOptions) {
     this.view = this.containerModal.view;
     return new Promise((resolve) => {
       this.setComponentModal(component, factoryResolver, null, null, mdOptions);
@@ -247,7 +247,7 @@ export class ModalService implements OnDestroy {
 
   private validateDataFormUpdate(component) {
     if (!component['dataForm']) {
-      this.createModalDialog(TlDialogInfo, component['factory'], null);
+      this.createModalDialog(TlDialogInfo, component['factory'], { title: component['recordNotFoundMessage']});
       this.componentInjected.instance.message = component['recordNotFoundMessage'];
       return false;
     }
@@ -499,7 +499,8 @@ export class ModalService implements OnDestroy {
   private confirmDelete(component: ModalInstance) {
     if (component.smartForm['executeAction'] === ActionsModal.DELETE) {
       this.referenceSmartForm = component;
-      this.createModalDialog(TlDialogConfirmation, this.referenceSmartForm.smartForm['factory'], null).then((value: any) => {
+      this.createModalDialog(TlDialogConfirmation, this.referenceSmartForm.smartForm['factory'],
+        { title: this.referenceSmartForm.smartForm['deleteConfirmationMessage']}).then((value: any) => {
         if (value.mdResult === ModalResult.MRYES) {
           this.handleSmartFormCallback(this.referenceSmartForm,
             {formResult: this.referenceSmartForm.smartForm['dataForm']});
